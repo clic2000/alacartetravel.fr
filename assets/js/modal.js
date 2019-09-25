30d376c990102c9a66d3cacece2909993f1b653c
Mathilde Blanchemanche <mathilde@clic2000.fr>
2019-09-25 15:28:03 +0200

    put all assets in a dedicated directory

---
+++ assets/js/modal.js
@@ -0,0 +1,104 @@
+function toggleModal() {
+    var element = document.getElementsByClassName("modal")[0];
+    element.classList.toggle("is-active");
+}
+
+function toggleDropdownAng() {
+    var element = document.getElementById("ang");
+    element.classList.toggle("is-active");
+}
+
+function toggleDropdownEsp() {
+    var element = document.getElementById("esp");
+    element.classList.toggle("is-active");
+}
+
+function toggleDropdownFr() {
+    var element = document.getElementById("fr");
+    element.classList.toggle("is-active");
+}
+
+function toggleDropdownHon() {
+    var element = document.getElementById("hon");
+    element.classList.toggle("is-active");
+}
+
+function toggleDropdownIrl() {
+    var element = document.getElementById("irl");
+    element.classList.toggle("is-active");
+}
+
+function toggleDropdownIt() {
+    var element = document.getElementById("it");
+    element.classList.toggle("is-active");
+}
+
+function toggleDropdownPor() {
+    var element = document.getElementById("por");
+    element.classList.toggle("is-active");
+}
+
+function toggleDropdownRep() {
+    var element = document.getElementById("rep");
+    element.classList.toggle("is-active");
+}
+
+function toggleDropdownSu() {
+    var element = document.getElementById("su");
+    element.classList.toggle("is-active");
+}
+
+function toggleDropdownEu() {
+    var element = document.getElementById("eu");
+    element.classList.toggle("is-active");
+}
+
+function toggleDropdownCa() {
+    var element = document.getElementById("ca");
+    element.classList.toggle("is-active");
+}
+
+function toggleDropdownBre() {
+    var element = document.getElementById("bre");
+    element.classList.toggle("is-active");
+}
+
+function toggleDropdownAr() {
+    var element = document.getElementById("ar");
+    element.classList.toggle("is-active");
+}
+
+function toggleDropdownChi() {
+    var element = document.getElementById("chi");
+    element.classList.toggle("is-active");
+}
+
+function toggleDropdownAfr() {
+    var element = document.getElementById("afr");
+    element.classList.toggle("is-active");
+}
+
+function toggleDropdownNz() {
+    var element = document.getElementById("nz");
+    element.classList.toggle("is-active");
+}
+
+function toggleDropdownAus() {
+    var element = document.getElementById("aus");
+    element.classList.toggle("is-active");
+}
+
+function toggleDropdownCroa() {
+    var element = document.getElementById("croa");
+    element.classList.toggle("is-active");
+}
+
+function toggleDropdownMont() {
+    var element = document.getElementById("mont");
+    element.classList.toggle("is-active");
+}
+
+function toggleDropdownCos() {
+    var element = document.getElementById("cos");
+    element.classList.toggle("is-active");
+}
